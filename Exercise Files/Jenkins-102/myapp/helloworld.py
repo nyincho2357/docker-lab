b91861e5cb61e9f1c07f9325e245e0d6dea9936f
Martine Nyincho <martinenyincho@Martines-MacBook-Pro.local>
2025-07-17 23:27:19 -0400

    Added advanced Dockerized Python app and Jenkins pipeline

--- Exercise Files/Jenkins-102/myapp/helloworld.py
+++ Exercise Files/Jenkins-102/myapp/helloworld.py
@@ -1,1 +1,10 @@
-print("Hello from Python app!")
+# myapp/helloworld.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello from Jenkins + Docker + Python!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
